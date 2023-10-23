238f3ea5d7b09e03f6a9423c0968de3d2c656458
Olga <olga@dmitrieva.ru>
2023-10-24 00:34:02 +0300

    Task 1 - Square multiplication check

---
+++ task1/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Write("Введите число: ");
+int x = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите второе число: ");
+int y = Convert.ToInt32(Console.ReadLine());
+if (y*y == x)
+{
+    Console.WriteLine("Первое число является квадратом второго числа");
+}
+else
+{
+    Console.Write("Не является квадратом");
+}
