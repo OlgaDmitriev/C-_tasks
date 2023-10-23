ab4e03b718410c1748fb7e9efd016c54ade42d13
Olga <olga@dmitrieva.ru>
2023-10-24 00:47:40 +0300

    Task 3 - Week day number

---
+++ task2/Program.cs
@@ -0,0 +1,31 @@
+﻿Console.Write("Введите номер дня недели: ");
+int x = Convert.ToInt32(Console.ReadLine());
+
+if (x == 1)
+{
+    Console.WriteLine("Понедельник");
+}
+if (x==2)
+{
+    Console.Write("Вторник");
+}
+if (x==3)
+{
+    Console.Write("Среда");
+}
+if (x==4)
+{
+    Console.Write("Четверг");
+}
+if (x==5)
+{
+    Console.Write("Пятница");
+}
+if (x==6)
+{
+    Console.Write("Суббота");
+}
+if (x==7)
+{
+    Console.Write("Воскресенье");
+}
