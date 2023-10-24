c4aeae0f9ab337f82fceb20712f9574d75a4a553
Olga <olga@dmitrieva.ru>
2023-10-24 11:40:23 +0300

    Task 5 - Показать целые числа в промежутке

---
+++ task5/Program.cs
@@ -0,0 +1,8 @@
+﻿System.Console.WriteLine("Введите число");
+int number = Convert.ToInt32(Console.ReadLine());
+int numberNegative = -number;
+while(numberNegative <= number)
+{
+    System.Console.WriteLine($"{numberNegative}");
+    numberNegative++;
+}
