40371d789e1331ba094d22f847c0e0713c702aa7
Olga <olga@dmitrieva.ru>
2023-10-24 11:56:12 +0300

    Задача 7 - последняя цифра трехзначного числа

---
+++ task7/Program.cs
@@ -0,0 +1,14 @@
+﻿// Написать программу, которая на вход принимает трехзначное число 
+// и на выходе показывает последнюю цифру этого числа.
+
+System.Console.WriteLine("Введите трехзначное число");
+int number = Convert.ToInt32(Console.ReadLine());
+if(number >=100 && number <= 999)
+{
+    int digitlast = number % 10;
+    System.Console.WriteLine(digitlast);
+}
+else
+{
+    System.Console.WriteLine("Вы ввели не трехзначное число");
+}
